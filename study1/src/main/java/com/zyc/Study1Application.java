02af9e6da7f30f8ff81e1d132f9821fc71a9eb5d
zhaopeng <zhaopeng@qujiali.com>
2018-09-14 11:39:50 +0800

    一个国际惯例的HelloWorld的实例

---
+++ study1/src/main/java/com/zyc/Study1Application.java
@@ -0,0 +1,12 @@
+package com.zyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study1Application.class, args);
+	}
+}
---
+++ study1/src/main/java/com/zyc/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.zyc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello World!";
+    }
+}
