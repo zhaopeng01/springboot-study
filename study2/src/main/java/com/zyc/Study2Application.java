a716ecae7ae6631c57e37c821f48a3ff68cc918a
zhaopeng <zhaopeng@qujiali.com>
2018-09-14 11:40:06 +0800

    主要是对IDEA工具的操作这里不做详细解释

---
+++ study2/src/main/java/com/zyc/Study2Application.java
@@ -0,0 +1,12 @@
+package com.zyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study2Application.class, args);
+    }
+}
