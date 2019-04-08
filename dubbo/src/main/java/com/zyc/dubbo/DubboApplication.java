e650865174e8d76b9588ea70cf10a67fdbc8c3ef
zhaopeng <v_huangxiao@baidu.com>
2019-04-08 16:07:23 +0800

    SpringBoot 整合kafka

---
+++ dubbo/src/main/java/com/zyc/dubbo/DubboApplication.java
@@ -0,0 +1,13 @@
+package com.zyc.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboApplication.class, args);
+    }
+
+}
